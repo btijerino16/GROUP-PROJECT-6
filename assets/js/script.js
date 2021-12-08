a558e5c8d8ff55159e490056a0d3237ca796bea1
Blake Georgii <blakegeorgii@gmail.com>
2021-12-08 14:41:04 -0800

    optimized youtubeApi function, made function to create Iframe

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,31 @@
-$(document).ready(function () {
+function createIframe(videoId){
+    var ifrm = document.createElement("iframe");
+        ifrm.setAttribute("src", ("https://www.youtube.com/embed/" + videoId));
+        ifrm.style.width = "640px";
+        ifrm.style.height = "480px";
+        document.body.appendChild(ifrm);
+}
 
+function youtubeApi(query) {
     var key = "AIzaSyD03GgsQRd4u-bPDRH7t_-yT9LEhAPUC5E";
-    var q = "ANY_TITLE_HERE";
+    var query = query;
     var URL = 'https://youtube.googleapis.com/youtube/v3/search?';
-
 
     var options = {
         part: 'snippet',
         key: key,
         maxResults: 20,
-        q: q,
+        q: query,
         type: "video"
     }
 
-    loadId();
+    $.getJSON(URL, options).then(function (data) {
+        var videoId = data.items[0].id.videoId;
+        console.log(videoId);
+        createIframe(videoId);
+    });
+}
 
-    function loadId() {
-        $.getJSON(URL, options, function (data) {
-            var videoId = data.items[0].id.videoId;
-            console.log(videoId);
-        });
-    }
-});
+youtubeApi("taylor swift");
+
 
