55e69b9cf101a7139083fc392cb3efbfbcbf949a
Michael Walker <michaelwwalker42@gmail.com>
2021-12-10 16:08:16 -0700

    added js to target search input and show video, and added a container to hold video

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,25 @@
+var searchInput = document.getElementById('search-it');
+var searchForm = document.getElementById('searchForm');
+var videoContainer = document.getElementById('videoContainer');
+
+function handleInputSubmit(event) {
+    event.preventDefault();
+    // get movie name from search input
+    var movieName = searchInput.value;
+    console.log("Movie name: " + movieName);
+
+    getMovieStars(movieName);
+
+}
+
 function createIframe(videoId) {
     var ifrm = document.createElement("iframe");
     apiURL = `https://www.youtube.com/embed/${videoId}`;
     ifrm.setAttribute("src", (apiURL));
     ifrm.style.width = "640px";
     ifrm.style.height = "480px";
-    document.body.appendChild(ifrm);
+    // document.body.appendChild(ifrm);
+    videoContainer.append(ifrm);
 }
 
 function youtubeApi(query) {
@@ -23,7 +38,7 @@ function youtubeApi(query) {
 
     $.getJSON(URL, options).then(function (data) {
         var videoId = data.items[0].id.videoId;
-        console.log(videoId);
+        console.log("Video ID: " + videoId);
         createIframe(videoId);
     });
 }
@@ -32,7 +47,7 @@ function youtubeApi(query) {
 
 
 function getMovieStars(movieQuery) {
-    var movieQuery = "Iron Man";
+    // var movieQuery = "Iron Man";
     var imdbApiKey = "k_ta4dd4a1";
     var titleApiURL = `https://imdb-api.com/en/API/SearchMovie/${imdbApiKey}/${movieQuery}`;
 
@@ -40,21 +55,33 @@ function getMovieStars(movieQuery) {
         .then(function (response) {
             response.json()
                 .then(function (data) {
-
+                    // get movie Id from data
                     var movieId = data.results[0].id;
                     console.log("Movie ID: " + movieId);
-
+                    // use movie id in api url
                     var idApiURL = `https://imdb-api.com/en/API/Title/${imdbApiKey}/${movieId}`;
-
+                    // get movie star from data
                     fetch(idApiURL)
                         .then(function (response) {
                             response.json()
                                 .then(function (data) {
                                     var star = data.actorList[0].name;
                                     console.log("Star: " + star);
+                                    // retrieve the videos from youtube for the movie star
+                                    youtubeApi(star);
                                 })
                         })
                 })
         })
+
 };
-getMovieStars();
+
+
+// Event listeners
+searchForm.addEventListener('submit', handleInputSubmit);
+
+
+
+
+
+
