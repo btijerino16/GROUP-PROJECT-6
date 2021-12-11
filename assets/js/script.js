2d6e9d9e28f3e424d0a4bb134158e21d85caaff1
btijerino16 <92263027+btijerino16@users.noreply.github.com>
2021-12-10 17:42:19 -0700

    search button

--- assets/js/script.js
+++ assets/js/script.js
@@ -58,3 +58,15 @@ function getMovieStars(movieQuery) {
         })
 };
 getMovieStars();
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.fixed-action-btn').floatingActionButton();
+  });
