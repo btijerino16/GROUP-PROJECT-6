b2dfecae9dcf2933fc0d3b8079d1c1ddf1465f4d
Michael Walker <michaelwwalker42@gmail.com>
2021-12-11 13:24:36 -0700

    add movie poster to page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,24 @@
 var searchInput = document.getElementById('search-it');
 var searchForm = document.getElementById('searchForm');
 var videoContainer = document.getElementById('videoContainer');
+var posterContainer = document.getElementById('posterContainer');
 
 
 function handleInputSubmit(event) {
     event.preventDefault();
     // clear out video container
     videoContainer.innerHTML = "";
+    // clear out poster container
+    posterContainer.innerHTML = "";
     // get movie name from search input
     var movieName = searchInput.value;
-    console.log("Movie name: " + movieName);
+    console.log("Movie name: " + movieName);    
 
-    searchInput.value = ""; 
+    // clear out search input
+    searchInput.value = "";
 
-    getMovieStars(movieName);
+
+    getMovieInfo(movieName);
 
 }
 
@@ -21,9 +26,14 @@ function createIframe(videoId) {
     var ifrm = document.createElement("iframe");
     apiURL = `https://www.youtube.com/embed/${videoId}`;
     ifrm.setAttribute("src", (apiURL));
-    ifrm.style.width = "640px";
-    ifrm.style.height = "480px";
+    // ifrm.style.width = "640px";
+    // ifrm.style.height = "480px";
     // document.body.appendChild(ifrm);
+    
+    // set video size to fit container with 4:3 ratio
+    ifrm.style.width = "100%";
+    ifrm.style.height = "75%";
+    // append iframe to video container
     videoContainer.append(ifrm);
 }
 
@@ -51,7 +61,7 @@ function youtubeApi(query) {
 // youtubeApi("taylor swift");
 
 
-function getMovieStars(movieQuery) {
+function getMovieInfo(movieQuery) {
     // var movieQuery = "Iron Man";
     var imdbApiKey = "k_ta4dd4a1";
     var titleApiURL = `https://imdb-api.com/en/API/SearchMovie/${imdbApiKey}/${movieQuery}`;
@@ -60,6 +70,7 @@ function getMovieStars(movieQuery) {
         .then(function (response) {
             response.json()
                 .then(function (data) {
+                    console.log(data);
                     // get movie Id from data
                     var movieId = data.results[0].id;
                     console.log("Movie ID: " + movieId);
@@ -69,11 +80,23 @@ function getMovieStars(movieQuery) {
                     fetch(idApiURL)
                         .then(function (response) {
                             response.json()
-                                .then(function (data) {
+                                .then(function (data) { 
+                                    // movie star
                                     var star = data.actorList[0].name;
                                     console.log("Star: " + star);
-                                    // retrieve the videos from youtube for the movie star
-                                    youtubeApi(star);                                    
+                                    // retrieve interview video from youtube for the movie star
+                                    youtubeApi(star + "interview");
+                                    
+                                    // movie poster
+
+                                    // create img element for movie poster
+                                    var poster = document.createElement("img");
+                                    // set the image source
+                                    poster.setAttribute("src", data.image);
+                                    // limit poster size to 100% of container
+                                    poster.style.maxWidth = "100%";                                    
+                                    // append the poster to container
+                                    posterContainer.append(poster);
                                 })
                         })
                 })
