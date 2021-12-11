d90656161a206b2dec925784e461dcb7d5a8a8ee
Michael Walker <michaelwwalker42@gmail.com>
2021-12-11 15:18:51 -0700

    save searches to local storage, dynamically create buttons for searches

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,23 +2,30 @@ var searchInput = document.getElementById('search-it');
 var searchForm = document.getElementById('searchForm');
 var videoContainer = document.getElementById('videoContainer');
 var posterContainer = document.getElementById('posterContainer');
+var pastSearches = document.getElementById('pastSearches');
+// movies array
+var movies = [];
 
+loadHistory();
 
 function handleInputSubmit(event) {
     event.preventDefault();
-    // clear out video container
-    videoContainer.innerHTML = "";
-    // clear out poster container
-    posterContainer.innerHTML = "";
+
     // get movie name from search input
     var movieName = searchInput.value;
-    console.log("Movie name: " + movieName);    
+    // if nothing is input, return
+    if (!movieName) {
+        return;
+    }
+    //console.log("Movie name: " + movieName);
 
     // clear out search input
     searchInput.value = "";
 
 
     getMovieInfo(movieName);
+
+    saveMovies(movieName);
 
 }
 
@@ -29,7 +36,7 @@ function createIframe(videoId) {
     // ifrm.style.width = "640px";
     // ifrm.style.height = "480px";
     // document.body.appendChild(ifrm);
-    
+
     // set video size to fit container with 4:3 ratio
     ifrm.style.width = "100%";
     ifrm.style.height = "75%";
@@ -61,32 +68,38 @@ function youtubeApi(query) {
 // youtubeApi("taylor swift");
 
 
-function getMovieInfo(movieQuery) {
-    // var movieQuery = "Iron Man";
+function getMovieInfo(movieQuery) {  
+
+    // clear out video container
+    videoContainer.innerHTML = "";
+    // clear out poster container
+    posterContainer.innerHTML = "";
+
     var imdbApiKey = "k_ta4dd4a1";
+    // template literal for api url with api key and movie title
     var titleApiURL = `https://imdb-api.com/en/API/SearchMovie/${imdbApiKey}/${movieQuery}`;
 
     fetch(titleApiURL)
         .then(function (response) {
             response.json()
                 .then(function (data) {
-                    console.log(data);
+                    //console.log(data);
                     // get movie Id from data
                     var movieId = data.results[0].id;
-                    console.log("Movie ID: " + movieId);
+                   // console.log("Movie ID: " + movieId);
                     // use movie id in api url
                     var idApiURL = `https://imdb-api.com/en/API/Title/${imdbApiKey}/${movieId}`;
                     // get movie star from data
                     fetch(idApiURL)
                         .then(function (response) {
                             response.json()
-                                .then(function (data) { 
+                                .then(function (data) {
                                     // movie star
                                     var star = data.actorList[0].name;
-                                    console.log("Star: " + star);
+                                   // console.log("Star: " + star);
                                     // retrieve interview video from youtube for the movie star
                                     youtubeApi(star + "interview");
-                                    
+
                                     // movie poster
 
                                     // create img element for movie poster
@@ -94,7 +107,7 @@ function getMovieInfo(movieQuery) {
                                     // set the image source
                                     poster.setAttribute("src", data.image);
                                     // limit poster size to 100% of container
-                                    poster.style.maxWidth = "100%";                                    
+                                    poster.style.maxWidth = "100%";
                                     // append the poster to container
                                     posterContainer.append(poster);
                                 })
@@ -104,9 +117,63 @@ function getMovieInfo(movieQuery) {
 
 };
 
+// save movies to local storage
+function saveMovies(movie) {
+    // prevent duplicates in array
+    if (movies.indexOf(movie) !== -1) {
+        return;
+    }
+
+    // push movie into movies array
+    movies.push(movie);
+    // save movies array to local storage
+    localStorage.setItem("movies", JSON.stringify(movies));
+    createHistoryBtns();
+};
+
+// create buttons for search history
+function createHistoryBtns() {
+    // clear out past searches div before running loop
+    pastSearches.innerHTML = "";
+    // loop over movies array
+    for (let index = 0; index < movies.length; index++) {
+        // create button for each movie in array
+        var pastSearchButton = document.createElement('button');
+        pastSearchButton.innerText = movies[index];
+        // set button class
+        pastSearchButton.setAttribute("class", "waves-effect waves-light btn");
+        // set button style
+        pastSearchButton.style.width = "100%";
+        pastSearchButton.style.margin = "2px";
+        // set button data attribute
+        pastSearchButton.setAttribute("data-value", movies[index]);
+        // append buttons to past searches div
+        pastSearches.append(pastSearchButton);
+    }
+}
+
+// load previous searches
+function loadHistory() {
+    // get movies array from local storage
+    var storedHistory = localStorage.getItem("movies");
+    // if there is data in local storage, parse the data
+    if (storedHistory) {
+        movies = JSON.parse(storedHistory);
+    }
+    createHistoryBtns();
+}
+
+
 
 // Event listeners
 searchForm.addEventListener('submit', handleInputSubmit);
+
+pastSearches.addEventListener('click', function (event) {
+
+    var pastMovieSearch = event.target.getAttribute('data-value');
+    //console.log(pastMovieSearch);
+    getMovieInfo(pastMovieSearch);
+})
 
 
 
